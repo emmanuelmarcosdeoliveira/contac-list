8de0ed148480159ca88a3cc40c4243c5f2b6ccdf
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-07-29 11:03:25 -0300

    created the contact filter by tag and name

--- src/components/FilterCard/index.tsx
+++ src/components/FilterCard/index.tsx
@@ -1,14 +1,46 @@
+import { alterarFiltro } from '../../store/reducers/filter'
 import * as S from './styles'
+import * as enums from '../../utils/enums/contatos'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
 export type CardProps = {
-  ativo?: 'ativado' | 'naoativado'
-  contador: number
   legenda: string
+  criterio: 'Trabalho' | 'Pessoal' | 'Familia' | 'Todas'
+  valor?: enums.Grupos
 }
 
-const FilterCard = ({ ativo, contador, legenda }: CardProps) => {
+const FilterCard = ({ legenda, criterio, valor }: CardProps) => {
+  const dispatch = useDispatch()
+  const { filtro, contatos } = useSelector((state: RootReducer) => state)
+  const verificaEstaAtivo = () => {
+    const mesmoCriterio = filtro.criterio === criterio
+    const mesmoValor = filtro.valor === valor
+    return mesmoCriterio && mesmoValor
+  }
+  const contarContatos = () => {
+    if (criterio === 'Todas') return contatos.itens.length
+    if (valor === enums.Grupos.FAMILY) {
+      return contatos.itens.filter((item) => item.tag === 'Familia').length
+    } else if (valor === enums.Grupos.GUYS) {
+      return contatos.itens.filter((item) => item.tag === 'Pessoal').length
+    } else if (valor === enums.Grupos.WORK) {
+      return contatos.itens.filter((item) => item.tag === 'Pessoal').length
+    }
+  }
+
+  const filtrar = () => {
+    dispatch(
+      alterarFiltro({
+        criterio,
+        valor
+      })
+    )
+  }
+  const contador = contarContatos()
+  const ativo = verificaEstaAtivo()
   return (
-    <S.Card ativo={ativo}>
+    <S.Card ativo={ativo} onClick={filtrar}>
       <S.Contador>{contador}</S.Contador>
       <S.Label>{legenda}</S.Label>
     </S.Card>
--- src/store/reducers/contacts.ts
+++ src/store/reducers/contacts.ts
@@ -1,54 +1,71 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import Contatos from "../../models/Contatos";
-import * as enums from "../../utils/enums/contatos"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Contatos from '../../models/Contatos'
+import * as enums from '../../utils/enums/contatos'
 
 type ContatosState = {
-  itens: Contatos[] 
+  itens: Contatos[]
 }
-
 
 const initialState: ContatosState = {
-itens : [
-  {
-    tag: enums.Grupos.WORK, 
-    titulo: 'EBAC',
-    telefone: '11-968336094',
-    email: 'ebac@gmail.com',
-    id: 1
-  },
-  {
-    tag: enums.Grupos.FAMILY, 
-    titulo: 'João',
-    telefone: '11-99999-9999',
-    email: 'joaoc@gmail.com',
-    id: 2
-  }, 
-  {
-    tag: enums.Grupos.GUYS, 
-    titulo: 'Vida',
-    telefone: '11-99999-9999',
-    email: 'vidac@gmail.com',
-    id: 3
-  }
-]
+  itens: [
+    {
+      tag: enums.Grupos.WORK,
+      titulo: 'EBAC',
+      telefone: '11-968336094',
+      email: 'ebac@gmail.com',
+      id: 1
+    },
+    {
+      tag: enums.Grupos.FAMILY,
+      titulo: 'João',
+      telefone: '11-99999-9999',
+      email: 'joaoc@gmail.com',
+      id: 2
+    },
+    {
+      tag: enums.Grupos.GUYS,
+      titulo: 'Vida',
+      telefone: '11-99999-9999',
+      email: 'vidac@gmail.com',
+      id: 3
+    },
+    {
+      tag: enums.Grupos.FAMILY,
+      titulo: 'Everton',
+      telefone: '11-99999-9999',
+      email: 'eventon@gmail.com',
+      id: 4
+    },
+    {
+      tag: enums.Grupos.FAMILY,
+      titulo: 'Elisa',
+      telefone: '11-99999-9999',
+      email: 'elisa@gmail.com',
+      id: 5
+    }
+  ]
 }
 
-const contactsSlice  = createSlice({
+const contactsSlice = createSlice({
   name: 'contacts',
-  initialState, 
-    reducers: {
+  initialState,
+  reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state.itens = state.itens.filter(contato => contato.id !== action.payload)
-    }, 
+      state.itens = state.itens.filter(
+        (contato) => contato.id !== action.payload
+      )
+    },
     editar: (state, action: PayloadAction<Contatos>) => {
-     const  indexContato =  state.itens.findIndex(contato => contato.id === action.payload.id)
-     if(indexContato >= 0 ) {
-      state.itens[indexContato] = action.payload
-     }
-    }  
+      const indexContato = state.itens.findIndex(
+        (contato) => contato.id === action.payload.id
+      )
+      if (indexContato >= 0) {
+        state.itens[indexContato] = action.payload
+      }
+    }
   }
 })
 
 
-export  const {remover, editar}  =   contactsSlice.actions
+export const { remover, editar } = contactsSlice.actions
 export default contactsSlice.reducer
